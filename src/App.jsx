f1b7b816d9ef65ac9f5da01cf8a56b904ce8b928
carlotesoro2003 <carlomtesoro@gmail.com>
2023-11-18 14:46:24 +0800

    todo using react practice

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState  } from "react"
+import "./style.css"
+import { NewTodoForm } from "./newTodoForm"
+import { TodoList } from "./TodoList"
+
+
+export default function App(){
+  const [todos, setTodos] = useState(() => {
+    const localvalue = localStorage.getItem("ITEMS")
+    if(localvalue == null){
+      return []
+    }
+    return JSON.parse(localvalue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+
+        if(todo.id == id){
+          return {...todo, completed}
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function addTodo(title){
+     setTodos((currentTodos) =>{
+      return [
+        ...currentTodos, {id: crypto.randomUUID(), title, completed: false},
+      ]
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id != id)
+    })
+  }
+
+  
+  return (
+  <>
+    <NewTodoForm  onSubmit={addTodo}/>
+    <h1 className="header">ToDo List</h1>
+    <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+  </>
+  )
+}
